ff582d72cd23eeb29daa5844848da0752e86aa2d
Mocanu Paul <mocanupaulc@gmail.com>
2022-10-10 10:08:53 +0200

    start spring application

---
+++ src/main/java/be/kdg/integration/plantifybackend/PlantifybackendApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.integration.plantifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantifybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantifybackendApplication.class, args);
+	}
+
+}
