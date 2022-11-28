4800d16bc52dbc287d9317de0a7f093efc9fc302
ElliotHermans <elliot.hermans@student.kdg.be>
2022-11-28 11:42:38 +0100

    CONTROLLER STUFF

--- src/main/java/be/kdg/integration/plantifybackend/presentation/PlantController.java
+++ src/main/java/be/kdg/integration/plantifybackend/presentation/PlantController.java
@@ -4,22 +4,29 @@ import be.kdg.integration.plantifybackend.domain.Arduino;
 import be.kdg.integration.plantifybackend.domain.Plant;
 import be.kdg.integration.plantifybackend.domain.PlantType;
 import be.kdg.integration.plantifybackend.domain.User;
+import be.kdg.integration.plantifybackend.presentation.viewModel.PlantViewModel;
 import be.kdg.integration.plantifybackend.service.ArduinoService;
 import be.kdg.integration.plantifybackend.service.PlantService;
 import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -30,6 +37,7 @@ public class PlantController {
     PlantService plantService;
     Gson gson = new Gson();
     ArduinoService arduinoService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public PlantController(PlantService plantService, ArduinoService arduinoService) {
@@ -107,5 +115,77 @@ public class PlantController {
 
 //    [[{"humidity":53,"temperature":24.4,"brightness":14.37147,"moisture":1}]]
 
+    //-----add plant controller
 
+    /**
+     * shows the addPlant.html page
+     * @param model attributes added:
+     *              - plantTypes: list of the types of plant for the select input
+     *              - plantViewModel: passes on the ViewModel for collecting data and validation
+     * @return addplant.html
+     */
+    @GetMapping("addPlant")
+    public String showAddPlant(Model model) {
+        model.addAttribute("loggedInOrNot",true);
+        List<PlantType> plantTypes = Arrays.stream(PlantType.values()).toList();
+        model.addAttribute("plantTypes", plantTypes);
+        model.addAttribute("add", "chill");
+        model.addAttribute("plantViewModel", new PlantViewModel());
+
+        return "addplant";
+    }
+
+
+    /**
+     * handles the form input
+     * @param httpSession used to retrieve the User
+     * @param plantViewModel used to retrieve the data from the form
+     * @param errors used to detect and retrieve any errors
+     * @param model used to
+     * @return returns addPlant.html when an error is detected server-side, otherwise returns plants
+     */
+    @PostMapping("addPlant")
+    public String addPlant(HttpSession httpSession, @Valid @ModelAttribute("plantViewModel") PlantViewModel
+            plantViewModel, BindingResult errors, Model model) {
+        if (errors.hasErrors()) {
+            errors.getAllErrors().forEach(error -> {
+                logger.error(error.toString());
+            });
+            List<PlantType> plantTypes = Arrays.stream(PlantType.values()).toList();
+            model.addAttribute("plantTypes", plantTypes);
+            model.addAttribute("plants", plantService.readPlants());
+            return "addPlant";
+        }
+
+        User user = (User) httpSession.getAttribute("user");
+        Arduino arduino = this.arduinoService.addArduino(plantViewModel.getArduinoSeries(), plantViewModel.getPhysicalAddress());
+//        this.arduinoService.addArduino(arduinoSeries,Integer.parseInt(psysicalAddress));
+        this.plantService.addPlant(plantViewModel.getName(), plantViewModel.getType(), arduino,user.getEmail());
+        return "redirect:/plants";
+    }
+
+
+    /**
+     *
+     * @param model used to pass remove and loggedInOrNot attributes
+     * @return removePlant.html
+     */
+    @GetMapping("removePlant")
+    public String showRemovePlant(Model model) {
+        model.addAttribute("remove");
+        model.addAttribute("loggedInOrNot",true);
+        return "removePlant";
+    }
+
+    /**
+     * removes the plant from the database
+     * @param plantId id of the plant that was selected to be removed
+     * @return dashboard.html
+     */
+    @PostMapping("removePlant")
+    public String removePlant(Integer plantId) {
+//        Arduino arduino = this.arduinoService.addArduino(arduinoSeries, Integer.parseInt(physicalId));
+        this.plantService.removePlant(plantId);
+        return "redirect:/plants";
+    }
 }
